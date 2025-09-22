a4770dc6eeb4a4d30f67e7a21234d27250a93708
weavermarquez <weavermarquez0@gmail.com>
2025-09-22 12:47:02 -0400

    fix: replace static svg with Canvas

--- tarati-react/src/App.tsx
+++ tarati-react/src/App.tsx
@@ -1,20 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useRef } from 'react'
 import dodecagonBoard from './assets/dodecagon.svg'
 import './App.css'
+
+const Canvas = props => {
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+
+    // Our first draw
+
+    context.fillStyle = '#000000'
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+  }, [])
+
+  return <canvas ref={canvasRef} {...props}/>
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
       <div>
-        <img src={dodecagonBoard} className="board" alt="Outer Board" />
+        { Canvas() }
       </div>
-
-      <h1>Tarati</h1>
     </>
   )
 }
